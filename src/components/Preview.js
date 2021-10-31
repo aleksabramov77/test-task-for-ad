61eb8a0152ee8fa582b3648f716c421699bf6d1e
Aleksandr Abramov <aleksabramov77@gmail.com>
2021-10-31 21:25:30 +0300

    09 ageToText

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,6 +1,23 @@
 import React from 'react'
 
 const Preview = ({ personData }) => {
+    const ageToText = (age) => {
+        let txt, count = age % 100;
+        if (count >= 5 && count <= 20) {
+            txt = 'лет';
+        } else {
+            count = count % 10;
+            if (count === 1) {
+                txt = 'год';
+            } else if (count >= 2 && count <= 4) {
+                txt = 'года';
+            } else {
+                txt = 'лет';
+            }
+        }
+        return `${age} ${txt}`;
+    }
+
     return (
         <div className='main__preview'>
 
@@ -8,7 +25,7 @@ const Preview = ({ personData }) => {
             <div className='info-block'>
                 <div className='info-block__title'>Персональные данные</div>
                 {personData.name && <div
-                    className='info-block__data info-block__data_person'>{personData.name + ', ' + personData.age + ' лет'}</div>}
+                    className='info-block__data info-block__data_person'>{personData.name + ', ' + ageToText(personData.age)}</div>}
             </div>
 
             {/* Children data display block */}
@@ -16,7 +33,7 @@ const Preview = ({ personData }) => {
                 {personData.children.length !== 0 && <div className='info-block__title '>Дети</div>}
                 <div className='info-block__data'>
                     {personData.children.map(child => <div className='info-block__data_child' key={child.id}>
-                        {child.name + ', ' + child.age + ' лет'}
+                        {child.name + ', ' + ageToText(child.age)}
                     </div>)}
                 </div>
             </div>
